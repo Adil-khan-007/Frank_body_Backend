70da213ca8d874b168e44cfcc1a166ac6af10b90
Adil-khan-007 <adilk9030@gmail.com>
2023-02-07 18:23:21 +0530

    AuthRouter done

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -1,65 +1,65 @@
 
-// const jwt = require("jsonwebtoken");
-// const { UserModel } = require("../configs/Model");
+const jwt = require("jsonwebtoken");
+const { UserModel } = require("../configs/Model");
 
-// const generateToken = (user)=>{
-//     const {email,name,age} = user;
-//     return jwt.sign({email,name,age},"mynameiskingkhan");
-// }
+const generateToken = (user)=>{
+    const {email,name,age} = user;
+    return jwt.sign({email,name,age},"mynameiskingkhan");
+}
 
-// const register = async (req,res)=>{
-//     try{
-//         const {email,password,name,age} = req.body;
+const register = async (req,res)=>{
+    try{
+        const {email,password,name,age} = req.body;
 
-//     if(!email || !password || !name){
-//        return res.status(500).send({message : "name, email and password are mandatory"})
-//     }
+    if(!email || !password || !name){
+       return res.status(500).send({message : "name, email and password are mandatory"})
+    }
 
-//     const exist = await UserModel.findOne({email});
+    const exist = await UserModel.findOne({email});
 
-//     if(exist){
-//        return res.status(404).send({message : "User with this email already exist"})
-//     }
+    if(exist){
+       return res.status(404).send({message : "User with this email already exist"})
+    }
 
-//     const user = await UserModel.create({name,email,password,age});
+    const user = await UserModel.create({name,email,password,age});
 
-//       res.send({message : "Registration Succesfully"})
-//     }
-//     catch(err){
-//        return res.status(500).send({message : "Something went wrong"} )
-//     }
+      res.send({message : "Registration Succesfully"})
+    }
+    catch(err){
+       return res.status(500).send({message : "Something went wrong"} )
+    }
 
-// }
+}
 
-// const login = async (req,res)=>{
-//     try{
-//         const {email,password} = req.body;
+const login = async (req,res)=>{
+    try{
+        const {email,password} = req.body;
 
-//     if(!email || !password){
-//        return res.status(500).send({message : "Email and password are mandotory"})
-//     }
+    if(!email || !password){
+       return res.status(500).send({message : "Email and password are mandotory"})
+    }
 
-//     const user = await UserModel.findOne({email});
+    const user = await UserModel.findOne({email});
 
-//     if(!user){
-//        return res.status(401).send({message : "User not exist with this credentials"})
-//     }
+    if(!user){
+       return res.status(401).send({message : "User not exist with this credentials"})
+    }
      
-//     const token = generateToken(user);
-//     const {name,age} = user
+    const token = generateToken(user);
+    const {name,age} = user
 
-//     return res.send({
-//         token : token,
-//         data : {name,age,email}
-//     })
+    return res.send({
+        token : token,
+        data : {name,age,email}
+    })
 
-//     }
-//     catch(err){
-//        return res.status(500).send({message : "Something went wrong"} )
-//     }
-// }
+    }
+    catch(err){
+       return res.status(500).send({message : "Something went wrong"} )
+    }
+}
 
-// module.exports = {
-//     register,
-//     login
-// }
+module.exports = {
+    register,
+    login
+}
